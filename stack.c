347a4dab7309aa0d0b4cb9341128d430dafde95c
my-revgithub <raghav.revankar@gmail.com>
2024-10-05 19:24:00 +0530

    Update stack.c
    Add isEmpty and stack size functions

--- stack.c
+++ stack.c
@@ -10,6 +10,30 @@
 	struct stack *next_ptr;
 };*/
 
+uint8 proc_stack_isEmpty(const struct stack **head)
+{
+	if(*head != NULL)
+	{return 0;}
+	else
+	{return 1;}
+}
+
+uint8 stack_size(const struct stack **head)
+{
+	struct stack *p1;
+	uint8 stack_len;
+	
+	p1 = (struct stack *)*head;
+	stack_len = 0;
+	while(p1 != NULL)
+	{
+		++stack_len;
+		p1 = p1->next_ptr;
+	}
+	
+	return stack_len;
+}
+
 void proc_stack_push(struct stack **head, uint8 data)
 {
 	struct stack *p1;
@@ -79,6 +103,7 @@ void print_stack (struct stack **head)
 void proc_stack(void)
 {
 	struct stack *s_head;
+	uint8 is_Empty;
 	
 	proc_stack_push(&s_head,10);
 	proc_stack_push(&s_head,20);
@@ -87,6 +112,12 @@ void proc_stack(void)
 	proc_stack_push(&s_head,50);
 	proc_stack_push(&s_head,60);
 	
+	printf("Stack size = %d\n", stack_size(&s_head));
+	is_Empty = proc_stack_isEmpty(&s_head);
+	if (!is_Empty)
+		printf("The stack is not empty\n");
+	else
+		printf("The stack is empty\n");
 	//printf("\n Stack contents :\n");
 	print_stack(&s_head);
 
@@ -100,5 +131,11 @@ void proc_stack(void)
 	print_stack(&s_head);
 	printf("Data popped %d\n", proc_stack_pop(&s_head));
 	proc_stack_pop(&s_head);
+
+	is_Empty = proc_stack_isEmpty(&s_head);
+	if (!is_Empty)
+		printf("The stack is not empty\n");
+	else
+		printf("The stack is empty\n");
 
 }
