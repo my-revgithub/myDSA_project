cbedfc9078ca7650d0e404978d0e33a6b8e9119e
my-revgithub <raghav.revankar@gmail.com>
2024-12-16 07:59:34 +0530

    Tree Structure
    Basic Example

---
+++ include/tree.h
@@ -0,0 +1,9 @@
+#ifndef __TREE_H__
+#define __TREE_H__
+
+#include "typedef.h"
+#include "linked_list.h"
+
+extern void proc_tree(void);
+
+#endif
---
+++ tree.c
@@ -0,0 +1,11 @@
+#include "typedef.h"
+#include "tree.h"
+
+void proc_tree(void)
+{
+	struct node *root;
+	
+	proc_insert_node2(&root, 10);
+	
+	
+}
